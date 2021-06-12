5089965f3c5a8d6272aac4768a76efb6bb2db9f0
izhre <izhrenat@mail.ru>
2021-06-12 20:03:46 +0300

    test

--- Hangman.py
+++ Hangman.py
@@ -3,8 +3,43 @@ import random
 f = open('word_rus.txt', 'r', encoding='utf-8')
 words = f.readlines()
 w = words[random.randint(1, 34010)]
-print(w)
+answer = list('_' * (len(w) - 1))
+flag = True
+lives = 6
+used = list()
 print('Your word:')
-print('_' * (len(w) - 1))
-print('Select a letter:')
-letter = input()
+print(''.join(answer))
+while flag:
+    fl = False
+    print('Select a letter:')
+    letter = input()
+    used.append(letter)
+    for a in range(len(w)):
+        if letter == w[a]:
+            answer[a] = letter
+            fl = True
+    if not fl:
+        lives -= 1
+    if lives == 0:
+        print('Your word:')
+        print(''.join(answer))
+        print('')
+        print('Lives:', lives)
+        print('')
+        print('You lose(')
+        print('Correct answer:', w)
+        flag = False
+    elif '_' not in answer:
+        print('Your word:')
+        print(''.join(answer))
+        print('')
+        print('Lives:', lives)
+        print('')
+        print('CONGRATULATIONS!')
+        flag = False
+    else:
+        print('Your word:')
+        print(''.join(answer))
+        print('')
+        print('Lives:', lives)
+        print('Used letters:', ' '.join(used))
